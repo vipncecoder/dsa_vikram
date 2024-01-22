0b96ce000c129c74767e4d7dbe4d1f2c53e4ce32
Vikram Kumar <153069596+vipncecoder@users.noreply.github.com>
2024-01-22 16:23:18 +0000

    intro to string

---
+++ dsa course/jan21.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class jan21 {
+    public static void main(String[] args) {
+        // System.out.println("vikram");
+        // introduction to Strings
+        // String sc="vikram kumar";
+        Scanner sc=new Scanner(System.in);
+
+        // declaration and initialisation of String
+        String str=" I am vikram kumar";
+        String str1=new String("hey there how are you");
+        // System.out.println(str1);
+        
+        
+        // Taking input of String
+        // String str2=sc.next();  // it will take only first string if we give line as input
+        // System.out.println(str2);
+
+        String str3=sc.nextLine(); //-- it will take whole line as input
+        System.out.println(str3);
+
+        // some inbuilt String function
+         
+    }
+}
+
