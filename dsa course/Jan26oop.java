b77ae3b9798d8974a5484c558eabf156b76eaa1a
Vikram Kumar <153069596+vipncecoder@users.noreply.github.com>
2024-01-26 17:42:20 +0000

    oops added

--- dsa course/Jan26oop.java
+++ dsa course/Jan26oop.java
@@ -10,6 +10,12 @@ class car{
     void moveAhead(){
         System.out.println("moving ahead");
     }
+    // void moveBackward(){
+    //     if(isOn==false){
+    //         System.out.println("start the car first");
+    //         return;
+    //     }
+    // }
     void stop(){
         System.out.println("stop the car");
     }
@@ -17,20 +23,43 @@ class car{
         return curYear-manufactureDate;
     }
 }
+class couple{
+    int first;
+    int second;
 
+    boolean equals(couple c){
+
+        // whether the first & second of this
+        // object is equals to the first & second
+        // of the object "c" or not?
+        return first ==c.first && second==c.second;
+    }
+    String convertToString(){
+        return "[" + first + ", " + second + " ]";
+    }
+}
 
 public class Jan26oop {
     public static void main(String[] args) {
-        car c1=new car();
+        // car c1=new car();
         // System.out.println(c1);
 
         //accessing the properties of c1
-        c1.manufactureDate=2018;
-        c1.color="white";
-        c1.start();
-        c1.moveAhead();
-        c1.stop();
-        System.out.println(c1.age(2024));
+        // c1.manufactureDate=2018;
+        // c1.color="white";
+        // c1.start();
+        // c1.moveAhead();
+        // c1.stop();
+        // System.out.println(c1.age(2024));
+
+        couple c1=new couple();
+        c1.first=1;
+        c1.second=5;
+        System.out.println(c1.convertToString());
+        couple c2= new couple();
+        c2.first=1;
+        c2.second=5;
+        System.out.println(c1.equals(c2));
 
     }
     
